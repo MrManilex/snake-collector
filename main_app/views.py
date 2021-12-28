d6b161eabe9f370fc1b5429af7a102833ac6b781
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-27 20:01:57 -0500

    set up home page

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-   return HttpResponse('<h1>your moms a hoe</h1>')
+   return render(request, 'base.html')
 
 def about(request):
    return render(request, 'about.html')
