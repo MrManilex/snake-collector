79d931df83b63b654fe33641b3fedc99f79e51d9
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-28 18:38:53 -0500

    require log in in all views

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from main_app.forms import FeedingForm
 from .models import Snake, Toy
 
@@ -14,8 +16,9 @@ def home(request):
 def about(request):
    return render(request, 'about.html')
 
+@login_required(login_url='/')
 def snakes_index(request):
-   snakes = Snake.objects.all()
+   snakes = Snake.objects.filter(user=request.user)
    return render(request, 'snakes/index.html', { 'snakes': snakes })
 
 def snakes_detail(request, snake_id):
@@ -44,14 +47,15 @@ def signup(request):
       if form.is_valid():
          user = form.save()
          login(request, user)
-         return redirect('cats_index')
+         return redirect('snakes_index')
       else:
          error_message = 'Invalid sign up - try again'
    form = UserCreationForm()
    context = {'form': form, 'error_message': error_message}
    return render(request, 'signup.html', context)
 
-class SnakeCreate(CreateView):
+class SnakeCreate(LoginRequiredMixin, CreateView):
+   login_url = '/'
    model = Snake
    fields = ['name', 'breed', 'description', 'age']
    
@@ -59,29 +63,36 @@ class SnakeCreate(CreateView):
       form.instance.user = self.request.user
       return super().form_valid(form)
 
-class SnakeUpdate(UpdateView):
+class SnakeUpdate(LoginRequiredMixin, UpdateView):
+   login_url = '/'
    model = Snake
    fields = ['breed', 'description', 'age']
    
-class SnakeDelete(DeleteView):
+class SnakeDelete(LoginRequiredMixin, DeleteView):
+   login_url = '/'
    model = Snake
    success_url = '/snakes/'
    
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
+   login_url = '/'
    model = Toy
    fields = '__all__'
    
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
+   login_url = '/'
    model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
+   login_url = '/'
    model = Toy
    
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+   login_url = '/'
    model = Toy
    fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
+   login_url = '/'
    model = Toy
    success_url = '/toys/'
    
