6bfafdd9e0c482608764e37914c311cfd6cf3b20
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-27 20:20:32 -0500

    create index page for snakes

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
    return render(request, 'base.html')
 
 def about(request):
    return render(request, 'about.html')
+
+def snakes_index(request):
+   return render(request, 'snakes/index.html', { 'snakes': snakes })
+
+
+class Snake:  # Note that parens are optional if not inheriting from another class
+   def __init__(self, name, breed, description, age):
+      self.name = name
+      self.breed = breed
+      self.description = description
+      self.age = age
+
+
+snakes = [
+   Snake('Lolo', 'tabby', 'Kinda rude.', 3),
+   Snake('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+   Snake('Fancy', 'bombay', 'Happy fluff ball.', 4),
+   Snake('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
